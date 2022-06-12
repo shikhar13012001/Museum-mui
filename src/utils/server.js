3747663bffa2c99cc91fb42efb33045543118c58
Shikhar <ishgupta2015@gmail.com>
2022-06-12 15:23:15 +0530

    ServerURL update

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,3 +1,3 @@
-const serverURL = "http://localhost:5000/api/user";
-// const serverURL="https://pure-bayou-11849.herokuapp.com/api/user";
+// const serverURL = "http://localhost:5000/api/user";
+const serverURL="https://pure-bayou-11849.herokuapp.com/api/user";
 export default serverURL;
